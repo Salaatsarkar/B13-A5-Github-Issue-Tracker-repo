d2acbd9cc51ecb6c44e716d87479d12f148739d6
Salaatsarkar <salaatsarkar0501@gmail.com>
2026-03-09 18:26:29 +0400

    card move done

--- script/github.js
+++ script/github.js
@@ -8,6 +8,9 @@ const tabInactive = ["bg-transparent", "text-neutral/50"];
 const allContainer = document.getElementById('btn-all');
 const openContainer = document.getElementById('btn-open');
 const closedContainer = document.getElementById('btn-close');
+const allCardContainer = document.getElementById('card-container');
+const openCardContainer = document.getElementById('open-card-container');
+const closeCardContainer = document.getElementById('close-card-container');
 
 //function for change tab
 function toggleTab(tab) {
@@ -27,12 +30,52 @@ function toggleTab(tab) {
             tabName.classList.add(...tabInactive);
         }
     }
+
+
+    //add hidden class in containers
+    const webpages = [allCardContainer, openCardContainer, closeCardContainer]
+    for(const webpage of webpages) {
+        webpage.classList.add('hidden');
+    }
+
+
+    //remove hidden class in click sections
+    if(tab === 'all') {
+        allCardContainer.classList.remove('hidden');
+    }
+    else if(tab === 'open'){
+        openCardContainer.classList.remove('hidden');
+    }
+    else{
+        closeCardContainer.classList.remove('hidden');
+    }
+
+
+
+
+
+
+
+
 };
 
 //group of html elements
 const createElements = (arr) =>{
     const htmlElements = arr.map((el)=>`
-    <span class="btn btn-secondary btn-soft rounded-full"> ${el}</span>`);
+    <span class="btn btn-secondary btn-soft text-sm  w-35 rounded-full
+    ${el === "bug" ? 'btn-error'
+    : el === "help wanted" ? 'btn-warning'
+    : el === "enhancement" ? 'btn-success'
+    : el === "documentation" ? 'btn-info'
+    : 'btn secondary'
+    }">
+    ${el === "bug" ? '<i class="fa-solid fa-bug"></i>'
+      : el === "help wanted" ? '<i class="fa-solid fa-life-ring"></i>'
+      : el === "enhancement" ? '<i class="fa-solid fa-burst"></i>'
+      : el === "documentation" ? '<i class="fa-solid fa-file"></i>'
+      : '<i class="fa-brands fa-jira"></i>'
+    }
+     ${el}</span>`);
     return htmlElements.join(" "); //array k string kore
 }
 
@@ -51,11 +94,10 @@ const displayCard = (cards) =>{
   // cardContainer.innerHTML = "";
     //2. get into every cards
     cards.forEach(card => {
-        //3. create element
+             //3. create element
         const newCard= document.createElement("div");
         newCard.innerHTML = `
-        
-              <div class="card-body p-5  shadow-lg space-y-2 rounded-xl border-t-4 ${card.status === 'open'? 'border-green-500' : 'border-purple-500'}">
+              <div class="card-body p-5  shadow-lg space-y-2 rounded-xl border-t-4 ${card.status === 'open'? 'border-green-500' : 'border-purple-500'} overflow-hidden">
                 <div class="flex justify-between items-center">
                  <img src="./assets/${card.status === 'open' ? 'Open-Status.png' : 'Closed- Status .png'}" alt="">
                     <button class="${card.priority === 'high'
@@ -66,8 +108,7 @@ const displayCard = (cards) =>{
                 </div>
                 <h2 class="font-semibold text-sm ">${card.title}</h2>
                 <p class="text-neutral/50 text-sm">${card.description}</p>
-                <div class="flex gap-2 ">
-                   
+                <div class="flex gap-1">
                     ${createElements(card.labels)}
                 </div>
                     <div class="divider -mx-5"></div>
@@ -77,10 +118,18 @@ const displayCard = (cards) =>{
         
         `;
         //4. append child
-        cardContainer.append(newCard);
+         cardContainer.appendChild(newCard);
+
+         const cloneCard = newCard.cloneNode(true);
+        card.status === "open"
+        ? openCardContainer.appendChild(cloneCard)
+        : closeCardContainer.appendChild(cloneCard)
+
+       
 
     })
-    
+
+   
 };
 loadCard();
  toggleTab(currentTab);
